35d7cd609335037c185c9321a0ac6773ff1b7825
Akasiek <kpomykala2002@gmail.com>
2023-07-21 21:21:39 +0200

    feat(welcome)

--- src/components/slides/WelcomeSlide.vue
+++ src/components/slides/WelcomeSlide.vue
@@ -1,17 +1,14 @@
 <template>
-  <section class="bg-primary-light w-full h-full font-bold flex flex-col justify-center text-primary-dark px-24">
-    <h1 class="text-5xl">Hi! I’m Kamil,</h1>
-    <p class="text-xl mt-2">a full stack web developer from Rzeszów, Poland</p>
-    <p class="text-sm font-light mt-2">Scroll down to see more ^_^</p>
+  <section class="bg-primary-light w-full h-full font-bold flex flex-col justify-center text-primary-dark relative pl-[5%]">
+    <div class="absolute left-[5%] w-[30rem] aspect-square rounded-full overflow-hidden">
+      <img src="/images/me.webp" alt="My self-portrait" class="h-full w-full object-center object-cover" />
+    </div>
+    <div
+      class="absolute left-[35%] top-[calc(50%+2.5rem)] -translate-y-1/2 px-6 z-10 bg-primary-light py-4 border-4 border-primary-dark shadow-[10px_10px_0_#201E1F] transition-all w-fit"
+    >
+      <h1 class="text-5xl">Hi! I’m Kamil,</h1>
+      <p class="text-xl mt-2">a full stack web developer from Rzeszów, Poland</p>
+      <p class="text-sm font-light mt-2">Scroll down to see more ^_^</p>
+    </div>
   </section>
 </template>
-
-<!--<script lang="ts">-->
-<!--import { Motion } from 'motion/vue';-->
-
-<!--export default {-->
-<!--  components: { Motion }-->
-<!--};-->
-<!--</script>-->
-
-<!--<Motion class="inline-block" :in-view="{ rotate: [0, -5, 20, -25, 15, -10, 0] }" :transition="{ delay: 0.5, duration: 1.4, easing: 'ease-out' }">👋</Motion>-->
