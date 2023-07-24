ef51f152bf053d387f4168080b5907bc3b1c519f
Akasiek <kpomykala2002@gmail.com>
2023-07-24 19:45:46 +0200

    feat: Error view and title

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,31 +2,47 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
 import ProjectList from '@/views/projects/ProjectList.vue';
 import { BlogPostList, BlogPostView } from '@/views/blog';
+import ErrorView from '@/views/ErrorView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'Home Page',
       component: HomeView,
     },
     {
       path: '/projects',
-      name: 'projectList',
+      name: 'Projects',
       component: ProjectList,
     },
     {
       path: '/blog',
-      name: 'blog',
+      name: 'Blog',
       component: BlogPostList,
     },
     {
       path: '/blog/:id',
-      name: 'blogPost',
+      name: 'Blog Post',
       component: BlogPostView,
     },
+    // Catch all other routes
+    {
+      path: '/:pathMatch(.*)*',
+      component: ErrorView,
+    },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = `Kamil Pomykała | Profolio`;
+
+  if (['Blog Post', 'Projects', 'Blog'].includes(String(to.name))) {
+    document.title = `Kamil Pomykała | Profolio - ${String(to.name)}`;
+  }
+
+  next();
 });
 
 export default router;
---
+++ src/views/ErrorView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import ErrorComponent from '@/components/ErrorComponent.vue';
+</script>
+
+<template>
+  <ErrorComponent secondary-text="Page could not be found..." />
+</template>
