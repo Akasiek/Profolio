bbec665f084772c2528ac3a1c62c86693b856d04
Akasiek <kpomykala2002@gmail.com>
2023-07-28 21:09:00 +0200

    style(welcome): 2xl screen fix

--- src/components/slides/WelcomeSlide.vue
+++ src/components/slides/WelcomeSlide.vue
@@ -1,16 +1,16 @@
 <template>
-  <section class="bg-primary-light w-full h-full font-bold flex flex-col justify-center text-primary-dark relative pl-[5%]">
+  <section class="relative flex h-full w-full flex-col justify-center bg-primary-light pl-[5%] font-bold text-primary-dark">
     <div
-      class="absolute left-0 px-8 sm:left-[5%] top-[calc(45%)] lg:top-[calc(50%-3rem)] w-full sm:w-[32rem] lg:w-96 xl:w-[30rem] -translate-y-1/2 overflow-hidden"
+      class="absolute left-0 top-[calc(45%)] w-full max-w-2xl -translate-y-1/2 overflow-hidden px-8 sm:left-[5%] sm:w-[32rem] lg:top-[calc(50%-3rem)] xl:w-[30rem] 2xl:w-[34rem]"
     >
-      <img src="/images/me.webp" alt="My self-portrait" class="h-full w-full object-center object-cover aspect-square rounded-full" />
+      <img src="/images/me.webp" alt="My self-portrait" class="aspect-square h-full w-full rounded-full object-cover object-center" />
     </div>
     <div
-      class="absolute sm:left-[25%] lg:left-[35%] top-[calc(68%)] lg:top-[calc(50%+6rem)] 2xl:top-[calc(50%+2.5rem)] -translate-y-1/2 px-3 md:px-4 lg:px-6 py-2 md:py-3 lg:py-4 mr-8 z-10 bg-primary-light border-2 md:border-4 border-primary-dark shadow-[10px_10px_0_#201E1F] transition-all w-fit"
+      class="absolute top-[calc(68%)] z-10 mr-8 w-fit -translate-y-1/2 border-2 border-primary-dark bg-primary-light px-3 py-2 shadow-[10px_10px_0_#201E1F] transition-all sm:left-[25%] md:border-4 md:px-4 md:py-3 lg:left-[35%] lg:top-[calc(50%+6rem)] lg:px-6 lg:py-4 2xl:left-[34rem] 2xl:top-[calc(50%+2.5rem)]"
     >
       <h1 class="text-3xl md:text-4xl lg:text-5xl">Hi! I’m Kamil,</h1>
-      <p class="text-base md:text-lg lg:text-xl mt-2">a full stack web developer from Rzeszów, Poland</p>
-      <p class="text-xs md:text-sm font-light mt-2">Scroll down to see more ^_^</p>
+      <p class="mt-2 text-base md:text-lg lg:text-xl">a full stack web developer from Rzeszów, Poland</p>
+      <p class="mt-2 text-xs font-light md:text-sm">Scroll down to see more ^_^</p>
     </div>
   </section>
 </template>
