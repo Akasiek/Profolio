efbbac36aa396b432c46fe43e68eb6b42c3d58b0
Kamil Pomykała <kpomykala2002@gmail.com>
2024-01-06 18:42:37 +0100

    feat: Animation on Load

--- src/components/NavSide.vue
+++ src/components/NavSide.vue
@@ -1,18 +1,18 @@
-<script setup lang="ts">
-import { useSlideStore } from "@/stores/slide";
-import { ref } from "vue";
+<script setup lang='ts'>
+import { useSlideStore } from '@/stores/slide';
+import { ref } from 'vue';
 
 const slideStore = useSlideStore();
 
 const showNav = ref(false);
 
 const links = [
-  { label: "Welcome", url: "#welcome" },
-  { label: "About", url: "#about" },
-  { label: "Technologies", url: "#tech" },
-  { label: "Projects", url: "#projects" },
-  { label: "Blog", url: "#blog" },
-  { label: "Contact", url: "#contact" }
+  { label: 'Welcome', url: '#welcome' },
+  { label: 'About', url: '#about' },
+  { label: 'Technologies', url: '#tech' },
+  { label: 'Projects', url: '#projects' },
+  { label: 'Blog', url: '#blog' },
+  { label: 'Contact', url: '#contact' }
 ];
 
 const handleNavigation = (index: number) => {
@@ -32,29 +32,31 @@ const handleNavigation = (index: number) => {
 </script>
 
 <template>
-  <div class="fixed left-0 right-0 top-0 flex justify-end bg-primary-light lg:hidden" @click="showNav = !showNav" style="z-index: 30">
-    <button class="cursor-pointer p-4">
-      <img src="/icons/menu.svg" class="h-8 md:h-12" alt="Open menu icon" />
-    </button>
-  </div>
-  <nav
-    class="fixed inset-0 z-30 block h-screen bg-primary-light text-primary-dark transition-transform duration-700 lg:static"
-    :class="showNav ? 'max-lg:translate-x-0' : 'max-lg:translate-x-full'"
-  >
-    <button class="fixed right-0 top-0 cursor-pointer p-4 lg:hidden" @click="showNav = !showNav">
-      <img src="/icons/close.svg" alt="" class="h-8 md:h-12" />
-    </button>
-    <div class="mx-auto flex h-full w-fit flex-col justify-center text-right text-2xl font-black">
-      <a
-        v-for="(link, index) in links"
-        :key="index"
-        :href="link.url"
-        @click="handleNavigation(index)"
-        class="py-3 transition-opacity hover:opacity-100"
-        :class="{ 'opacity-100': slideStore.currentSlide === index, 'opacity-50': slideStore.currentSlide !== index }"
-      >
-        {{ link.label }}
-      </a>
+  <div>
+    <div class='fixed left-0 right-0 top-0 flex justify-end bg-primary-light lg:hidden' @click='showNav = !showNav' style='z-index: 30'>
+      <button class='cursor-pointer p-4'>
+        <img src='/icons/menu.svg' class='h-8 md:h-12' alt='Open menu icon' />
+      </button>
     </div>
-  </nav>
+    <nav
+      class='fixed inset-0 z-30 block h-screen bg-primary-light text-primary-dark transition-transform duration-700 lg:static'
+      :class="showNav ? 'max-lg:translate-x-0' : 'max-lg:translate-x-full'"
+    >
+      <button class='fixed right-0 top-0 cursor-pointer p-4 lg:hidden' @click='showNav = !showNav'>
+        <img src='/icons/close.svg' alt='' class='h-8 md:h-12' />
+      </button>
+      <div class='mx-auto flex h-full w-fit flex-col justify-center text-right text-2xl font-black'>
+        <a
+          v-for='(link, index) in links'
+          :key='index'
+          :href='link.url'
+          @click='handleNavigation(index)'
+          class='py-3 transition-opacity hover:opacity-100'
+          :class="{ 'opacity-100': slideStore.currentSlide === index, 'opacity-50': slideStore.currentSlide !== index }"
+        >
+          {{ link.label }}
+        </a>
+      </div>
+    </nav>
+  </div>
 </template>
